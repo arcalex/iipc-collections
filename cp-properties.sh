50898845cc38edd6abc902318ae86795085fcddf
Youssef Eldakar <youssefeldakar@gmail.com>
2022-09-08 13:02:16 +0200

    Rename webapp.baseurl to webapp.prefix

--- cp-properties.sh
+++ cp-properties.sh
@@ -7,6 +7,6 @@ for i in *; do
 
   sudo -u $i sed -i "s,^\(solr\.server\)=.*,\1=http://localhost:8983/solr/$i/," $i/solrwayback.properties
   sudo -u $i sed -i "s,^\(wayback\.baseurl\)=.*,\1=https://iipc-collections.bibalex.org/solrwayback/$i/solrwayback/," $i/solrwayback{,web}.properties
-  sudo -u $i sed -i "/^wayback\.baseurl=/awebapp.baseurl=/solrwayback/$i/solrwayback/" $i/solrwaybackweb.properties
+  sudo -u $i sed -i "/^wayback\.baseurl=/awebapp.prefix=/solrwayback/$i/solrwayback/" $i/solrwaybackweb.properties
   sudo -u $i sed -i "s,^\(chrome\.command\)=.*,\1=/usr/lib64/chromium-browser/headless_shell," $i/solrwayback.properties
 done
